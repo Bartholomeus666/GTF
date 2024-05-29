4c4cb42df34bf88b0a5563262580beec92595eab
MooniPuss <immanuel.claerhout999@gmail.com>
2024-05-29 19:54:38 +0200

    Commit scores

--- Grand Theft Fridge/Assets/Scripts/GameScene/Scores/ScoreManager.cs
+++ Grand Theft Fridge/Assets/Scripts/GameScene/Scores/ScoreManager.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public int[] Points = new int[4];
     public TMP_Text[] Scores = new TMP_Text[4];
+    public Image[] Positions = new Image[4];
+    public Sprite[] PositionsSprites = new Sprite[4];
     //public GameObject[] ScoreTriggers = new GameObject[1];
 
     private ScoreTrigger _scoreScript;
+
+    private int _playersBetter;
 
     private void Awake()
     {
@@ -22,7 +27,24 @@ public class ScoreManager : MonoBehaviour
         {
             //_scoreScript = ScoreTriggers[i].gameObject.GetComponent<ScoreTrigger>();
 
-            Scores[i].text = "Score: " + Points[i];
+            Scores[i].text = ($"{Points[i]}/5");
+        }
+        CheckPositions();
+    }
+
+    private void CheckPositions()
+    {
+        for (int i = 0; i < Points.Length; i++)
+        {
+            _playersBetter = 0;
+            for (int j = 0; j < Points.Length; j++)
+            {
+                if (Points[i] < Points[j])
+                {
+                    _playersBetter++;
+                }
+            }
+            Positions[i].sprite = PositionsSprites[_playersBetter];
         }
     }
 }
